2f832b02f7422be57a6c350b17d3d17be23d8001
李如欣 <461132172@qq.com>
2016-08-12 09:48:36 +0800

    Initial Commit

---
+++ GetImageColor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetImageColor
+//
+//  Created by Shorigo on 16/8/12.
+//  Copyright © 2016年 Shorigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
